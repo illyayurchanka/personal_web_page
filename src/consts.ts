15876b70944b43dae0bde3517adeecee7aec8840
KUWE <36008372+illyayurchanka@users.noreply.github.com>
2026-03-05 17:11:28 +0100

    Update SITE_TITLE from 'John Pepperon Blog' to 'Illya Blog'

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.blog
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'John Pepperon Blog';
+export const SITE_TITLE = 'Illya Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
 export const YOUTUBE_VIDEO = 'https://www.youtube.com/watch?v=QmV14LtCBRY&pp=ygUVZXhwb2xzaW9uIGdyZWVuc2NyZWVu';
 export const DONATION_LINK = "https://buymeacoffee.com/kuwe";
